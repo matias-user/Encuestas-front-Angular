46fecc0cb901023f751e0a5517d343f746d16739
Mati <matias.abarca2018@gmail.com>
2021-10-15 21:40:11 -0300

    Fachada de home ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
